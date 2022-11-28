cfe807ad2095c9a4838135a83d33a7d3b4bf7a47
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2022-11-28 17:20:47 +0300

    Initial Commit

---
+++ StoreApp/StoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by Atakan Kömürcü on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
